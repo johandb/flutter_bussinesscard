ff5019772272cac8094b31717b2cc1402d4c367c
Johandenboer <johan.den.boer@ict.nl>
2022-08-31 08:28:44 +0200

    - initial version

---
+++ android/app/src/main/java/com/jdb/flutter_businesscard/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jdb.flutter_businesscard;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/johan.jpg'),
+            ),
+            const Text('Johan den Boer', style: TextStyle(fontSize: 40.0, fontFamily: 'Pacifico', color: Colors.white)),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(fontFamily: 'Source Sans Pro', color: Colors.teal.shade100, fontSize: 20.0, letterSpacing: 2.5),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            const Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.teal),
+                title: Text(
+                  '+31(0) 6 249 00 477',
+                  style: TextStyle(fontFamily: 'Source Sans Pro', fontSize: 20.00),
+                ),
+              ),
+            ),
+            SizedBox(width: 10.0),
+            const Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal),
+                title: Text(
+                  'johanj.denboer@gmail.com',
+                  style: TextStyle(fontFamily: 'Source Sans Pro', fontSize: 20.00),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
